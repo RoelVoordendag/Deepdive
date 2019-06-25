3c7cdfd1e40617109d79a25bd0c12618acc08442
Roel Voordendag <rvoordendag@gmail.com>
2019-06-25 22:55:04 +0200

    `no progress

--- nestedDive/getHighest.py
+++ nestedDive/getHighest.py
@@ -1,30 +1,92 @@
 
-def sum(orignalData, layers):
-    print(orignalData)
-    for x in range(layers):
-        for name, data in orignalData.items():
-            print(name)
-            print(data)
-        print(x)
-    return "werkt de return zoals gemoeten"
+# def sum(orignalData, layers):
+#     allData = {}
+#     for x in range(layers):
+#         for name, data in orignalData.items():
+#             if x == layers - 1:
+#                 allData[name] = data
+
+#             orignalData = data
+    
+#     print(allData)
+#     for name, data in allData.items():
+#         print(name)
+#         print(data)     
+                
+                
+#     return "werkt de return zoals gemoeten"
+from collections import OrderedDict
+
+def sumData(data, layers):
+    allData = {}
+
+    # Get the latest item
+    for name, value in data.items():
+        for name, value in value.items():
+            for name, value in value.items():
+                # I have the data here`
+                wijkData = []
+
+                for nameData, data in value.items():
+                    wijkData.append(data)
+                calculatedData = sum(wijkData)
+                allData.update({name : calculatedData})
+            pass
+        pass
+
+    print(allData)    
+    # Sort the array/dict
+    sorted_x = sorted(allData.items(), key=lambda x: x[1], reverse = True)
+    # Change
+    sorted_dict = dict(OrderedDict(sorted_x)) #Force to dictionary for mongodb
+
+    print("sorted")
+    print(sorted_dict)
+
+def testSum(data, layers):
+    allData = {}
+    testing = []
+    # Make data to one big list
+    for name, value in data.items():
+        testing.append(value)
+
+    # for x in range(layers):
+    #     print(x)
+    #     for name, value in testing.items():
+    #         wijkData = []
+    #                                                                     # if x == layers - 1:
+    #                                                                         # print(value)
+    #                                                                     #     print("throug if")
+    #                                                                     #     for nameData, data in value.items():
+    #                                                                     #         wijkData.append(data)
+    #                                                                     #     calculatedData = sum(wijkData)
+    #                                                                     #     allData.update({name : calculatedData})     
+    #         testing.append(value)
+    #         print(value)
+    #     data = value
+
+
+    # print(wijkData)
+    # print(allData)
+
 
 
 testData = {
     "Charlois": {
             "Tarwewijk" : {
-                "data": {
+                "test": {
                     "Veiligheidsindex" : 73,
                     "Veiligheidsindex -subjectief" : 100
                 }
             },
             "Carnisse" : {
-                "data": {
+                "32": {
                     "Veiligheidsindex" : 50,
                     "Veiligheidsindex -subjectief" : 102
                 }
             },
             "ss" : {
-                "data": {
+                "32": {
                     "Veiligheidsindex" : 60,
                     "Veiligheidsindex -subjectief" : 60
                 }
@@ -32,20 +94,19 @@ testData = {
     },
     "testing": {
             "wijk1" : {
-                "data": {
+                "123123": {
                     "Veiligheidsindex" : 73,
                     "Veiligheidsindex -subjectief" : 50
                 }
             },
             "wijk2" : {
-                "id": 71,
-                "data": {
+                "123123123": {
                     "Veiligheidsindex" : 50,
                     "Veiligheidsindex -subjectief" : 100
                 }
            },
-    "ss" : {
-                "data": {
+            "ss" : {
+                "1212": {
                     "Veiligheidsindex" : 60,
                     "Veiligheidsindex -subjectief" : 100
                 }
@@ -54,4 +115,4 @@ testData = {
 }
 
 
-sum(testData,3)
+testSum(testData,3)
